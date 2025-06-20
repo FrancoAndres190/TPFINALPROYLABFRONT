74579b82d17f46e41386a47d179541fd6d31cd5c
FrancoAndres190 <francoandresligorria190@gmail.com>
2025-06-20 03:07:43 -0300

    Main

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./components/AuthContext.tsx";
+import App from "./App.tsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "./index.css";
+import "animate.css";
 
 //Aqui se genera el renderizado de la aplicacion mediante el uso de ReactDOM
 //Con la etiqueta App
